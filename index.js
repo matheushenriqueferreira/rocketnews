df4767ec39947e8597d86c5d4fd9b1671871f09c
Matheus <maatheus27silva@gmail.com>
2022-08-04 22:55:51 -0300

    Inserido eventListener no form
    Cancela o evento submit e exibe um alert

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 window.onload = function() {
+  const form = document.querySelector("form");
+  const email = document.querySelector("#email");
   
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();//Cancela o evento submit
+    alert(`O seu e-mail: ${email.value} foi cadastrado com sucesso no RocketNews.`);
+  });
 }
